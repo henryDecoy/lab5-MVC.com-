02b925e6cfaa3e9d1203b040c29668bf2d3e1808
henryDecoy <enriquesalinas1603@gmail.com>
2022-11-07 09:07:11 -0500

    Primera version del laboratorio 5 :3

---
+++ utils/seg.php
@@ -0,0 +1,28 @@
+<?php 
+
+    if(session_status()==1)session_start();
+
+    class seg{
+        public static function codificar($p){
+            return base64_encode(base64_encode($p));
+        }
+
+        public static function decodificar($p){
+            return base64_decode(base64_decode($p));
+        }
+        public static function GetToken(){
+            if (isset($_SESSION["token"]))
+                return $_SESSION["token"];
+            
+            $token= sha1(random_bytes(100));
+            $_SESSION["token"]=$token;
+            return $token;
+        }
+        public static function ValidaSesion($token){
+            if(!isset($token) | !hash_equals($token,$_SESSION["token"]))
+                return false;
+            
+                return true;
+        }
+    }
+?>
---
+++ views/principal/index.php
@@ -0,0 +1,9 @@
+
+    <div class="container-md">
+        <h1>Pagina principal</h1>
+
+        <p>sitio prueba</p>
+    </div>
+ 
+
+
